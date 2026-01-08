f29ab8cded821ccbb285c380effc4d987f0cba54
hunnx27 <hunnx27@i-screammedia.com>
2026-01-08 17:36:20 +0900

    add: API 응답 값 예시 주석 추가

--- src/main/java/com/kongju/backend/dashboard/controller/DashboardController.java
+++ src/main/java/com/kongju/backend/dashboard/controller/DashboardController.java
@@ -22,17 +22,68 @@ public class DashboardController {
 
     @Autowired
     private DashboardService dashboardService;
+
+    /*
+    # 값이 없는 경우
+    '공백'
+
+    # 값이 있는 경우
+    {
+        "id": 1,
+        "equipmentId": "EQ-001",
+        "temperature": 40,
+        "pressure": 52,
+        "vibration": 52,
+        "speed": 32,
+        "timestamp": "2026-01-08T17:24:30",
+        "timestampMs": 2026,
+        "createdAt": "2026-01-08T17:24:30.737"
+    }
+     */
     @RequestMapping("/api/dashboard/sensor-latest")
     public SensorLogEntity getLatestSensor(){
         SensorLogEntity latestSensor = dashboardService.getLatestSensor();
         return latestSensor;
     }
 
+    /*
+    응답 예제
+    # 값이 없는 경우
+    []
+
+    # 값이 있는 경우
+    [
+        ["2026-01-08 17:20",11],
+        ["2026-01-08 17:21",32],
+        ["2026-01-08 17:22",125],
+        ["2026-01-08 17:23",4],
+        ["2026-01-08 17:24",10],
+        ["2026-01-08 17:25",3],
+        ["2026-01-08 17:26",50],
+    ]
+     */
     @RequestMapping("/api/dashboard/production-chart")
     public List<Object[]> getProductionChart(){
         return dashboardService.getProductionChart();
     }
 
+    /*
+    # 값이 없는 경우
+    {
+        "equipmentStatus": "RUN",
+        "todayProduction": 0,
+        "activeAlarmCount": 3,
+        "defectRate": 13.7
+    }
+
+    # 값이 있는 경우
+    {
+        "equipmentStatus": "RUN",
+        "todayProduction": 3,
+        "activeAlarmCount": 3,
+        "defectRate": 13.7
+    }
+     */
     @RequestMapping("/api/dashboard/summary")
     public DashboardSummaryResponse getSummary(){
         return dashboardService.getSummary();
